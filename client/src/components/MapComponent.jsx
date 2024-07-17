91b102ac664a7debaaa7a6fc61a86f2c2b815d94
RehanAjiNarwindo <rhnajin@gmail.com>
2024-07-17 15:00:03 +0700

    map implementation

---
+++ client/src/components/MapComponent.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import {
+  Circle,
+  FeatureGroup,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+} from "react-leaflet";
+
+function MapComponent() {
+  const [position, setPosition] = useState();
+  navigator.geolocation.watchPosition((position) => {
+    setPosition([position.coords.latitude, position.coords.longitude]);
+  });
+
+  return (
+    <>
+      {position && (
+        <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </>
+  );
+}
+
+export default MapComponent;
