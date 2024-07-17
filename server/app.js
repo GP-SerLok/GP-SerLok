607b7af7d907e8f7875a37ad605c0c0f92347416
Lutfi Farhan Hakim <lutfifarhan23@gmail.com>
2024-07-17 10:25:11 +0700

    feat: login google

--- server/app.js
+++ server/app.js
@@ -1,16 +1,19 @@
 require('dotenv').config()
 const express = require('express')
-const User = require('./controller/user')
+const userController = require('./controller/userController')
 const app = express()
 const port = 3000
+var cors = require('cors')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.post('/login/google', User.googleLogin)
+app.post('/login')
+app.post('/login/google', userController.googleLogin)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
