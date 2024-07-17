ade512d3dd6db68772779b5858d082d788bb491e
RehanAjiNarwindo <rhnajin@gmail.com>
2024-07-18 01:34:57 +0700

    costum marker

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -8,12 +8,22 @@ import {
   TileLayer,
   useMap,
 } from "react-leaflet";
+import L from "leaflet";
 
 function MapComponent({ users }) {
   const [position, setPosition] = useState();
   navigator.geolocation.watchPosition((position) => {
     setPosition([position.coords.latitude, position.coords.longitude]);
   });
+
+  // aturan marker
+  const markerIcon = new L.Icon({
+    iconUrl: "../../public/redMarker.png",
+    iconSize: [40, 40],
+    iconAnchor: [17, 30],
+    popupAnchor: [1, -30],
+  });
+  const name = localStorage.getItem("name");
 
   return (
     <>
@@ -23,13 +33,17 @@ function MapComponent({ users }) {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {users.map((el, i) => {
-            return (
+          {users.map((el, i) =>
+            name === el.name ? (
+              <Marker key={i} position={el.position} icon={markerIcon}>
+                <Popup>{el.name}</Popup>
+              </Marker>
+            ) : (
               <Marker key={i} position={el.position}>
                 <Popup>{el.name}</Popup>
               </Marker>
-            );
-          })}
+            )
+          )}
         </MapContainer>
       )}
     </>
