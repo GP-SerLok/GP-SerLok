0096915aae328908359faa4bd4007ecf868414e6
RehanAjiNarwindo <rhnajin@gmail.com>
2024-07-18 12:04:10 +0700

    zoom map fixs

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -29,7 +29,7 @@ function MapComponent({ users }) {
   return (
     <>
       {position && (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+        <MapContainer center={position} zoom={20} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
